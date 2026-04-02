edbd00bf622f9f44ab33c60c170715f66788f386
osamahabib5 <54497174+osamahabib5@users.noreply.github.com>
2026-04-02 11:14:43 -0400

    updated app.js

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -6,8 +6,8 @@ import Chatbot from './components/Chatbot';
 import DocumentList from './components/DocumentList';
 import FamilyTree from './components/FamilyTree';
 
-// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
-const API_URL = process.env.REACT_APP_API_URL || '/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+
 
 function App() {
   const [activeTab, setActiveTab] = useState('chat');
